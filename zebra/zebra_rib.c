97a4c3acdbc9ac8d82044eb32fff9ea18584bedc
Matthias Ferdinand <mf@14v.de>
2011-12-26 16:35:30 +0400

    zebra: fix ifindex test condition (BZ#487)
    When the same ip address is used on several interfaces,
    and one of them gets deleted (or equivalent: set to down and
    then address removed), rib_delete_ipv[46] will also remove
    the connected route from other interfaces.
    
    rib_delete_ipv[46] is called twice when an interface is
    deleted:
        - for the "ifdown" event
        - for the address removal
    (note: this may be specific to the netlink interface of linux)
    The second call does not find the connected route to that same
    ifindex anymore, but deletes similar connected routes to any
    other ifindex instead.
    
    Reason: the ifindex check is on the same level as the check
    for ZEBRA_ROUTE_CONNECT/NEXTHOP_TYPE_IFINDEX. If everything
    matches except for the ifindex, the "else" part (intended for
    different route types) is executed, thus removing the route
    from the wrong interface.
    
    fix: move ifindex check inside the "then" part of the check
    for ZEBRA_ROUTE_CONNECT/NEXTHOP_TYPE_IFINDEX. Now connected
    routes to other ifindexes will not spill over to the "else"
    part for different route types anymore.

--- zebra/zebra_rib.c
+++ zebra/zebra_rib.c
@@ -1889,8 +1889,10 @@ rib_delete_ipv4 (int type, int flags, struct prefix_ipv4 *p,
       if (rib->type != type)
 	continue;
       if (rib->type == ZEBRA_ROUTE_CONNECT && (nexthop = rib->nexthop) &&
-	  nexthop->type == NEXTHOP_TYPE_IFINDEX && nexthop->ifindex == ifindex)
+	  nexthop->type == NEXTHOP_TYPE_IFINDEX)
 	{
+	  if (nexthop->ifindex != ifindex)
+	    continue;
 	  if (rib->refcnt)
 	    {
 	      rib->refcnt--;
@@ -2437,8 +2439,10 @@ rib_delete_ipv6 (int type, int flags, struct prefix_ipv6 *p,
       if (rib->type != type)
         continue;
       if (rib->type == ZEBRA_ROUTE_CONNECT && (nexthop = rib->nexthop) &&
-	  nexthop->type == NEXTHOP_TYPE_IFINDEX && nexthop->ifindex == ifindex)
+	  nexthop->type == NEXTHOP_TYPE_IFINDEX)
 	{
+	  if (nexthop->ifindex != ifindex)
+	    continue;
 	  if (rib->refcnt)
 	    {
 	      rib->refcnt--;
